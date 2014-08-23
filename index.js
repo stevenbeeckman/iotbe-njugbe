3c0f01b6f8f2813b9eed69761cfd9c2e8be5fd9c
sbeeckman <steven.beeckman+bitbucket@gmail.com>
2014-08-23 15:43:50 +0200

    Added some hapi.js routes.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(process.env.PORT || 3000);
 
+var sensors = new Array();
+sensors[0] = {id: 0, name: 'Photo Resistor', measurements: new Array()};
+
 server.route({
 	method: 'GET'
 	    , path: '/'
@@ -8,6 +11,46 @@ server.route({
 	    reply('Hello, Internet of Things fans!');
 	}
     });
+
+server.route({
+	method: 'POST'
+	    , path: '/sensor/{sensor_id}/measurement'
+	    , config: {
+	    handler: function(request, reply){
+		var newMeasurement = new Object();
+		newMeasurement.created_on = new Date();
+		newMeasurement.value = req.payload.value;
+		sensors[request.params.sensor_id].measurements.push(newMeasurement);
+		reply(newMeasurement);
+	    }
+	}
+});
+
+server.route({
+	method: 'GET'
+	    , path: '/sensor/{sensor_id}/measurements'
+	    , handler: function(request, reply){
+	    reply(sensors[req.params.sensor_id].measurements);
+	}
+});
+
+server.route({
+	method: 'GET'
+	    , path: '/sensors'
+	    , handler: function(request, reply){
+	    reply(sensors);
+	}
+});
+
+server.route({
+	method: 'GET'
+	    , path: '/sensor/{id}'
+	    , handler: function(request, reply){
+	    reply(sensors[req.params.id]);
+	}
+});
+
+
 
 server.start(function(){
 	console.log('Server running at:' + server.info.uri);
