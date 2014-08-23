c741f44e44d38ebd81a44d321a68eada005f69e9
sbeeckman <steven.beeckman+bitbucket@gmail.com>
2014-08-23 14:38:12 +0200

    Added the root route.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(3000);
 
+server.route({
+	method: 'GET'
+	    , path: '/'
+	    , handler: function(request, reply){
+	    reply('Hello, Internet of Things fans!');
+	}
+);
+
 server.start(function(){
 	console.log('Server running at: server.info.uri');
 });
