874a341935711d0c3b495601f775882a86954ab4
sbeeckman <steven.beeckman+bitbucket@gmail.com>
2014-08-23 14:54:08 +0200

    Fixed typo.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ server.route({
 	    , handler: function(request, reply){
 	    reply('Hello, Internet of Things fans!');
 	}
-);
+    });
 
 server.start(function(){
 	console.log('Server running at: server.info.uri');
